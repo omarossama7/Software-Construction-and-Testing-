0d180c04f38abdda34b0d81794fd71e7174581a8
HusseinSelim-1977 <husseinsherif2006@gmail.com>
2025-12-03 00:00:42 +0200

    Modified: app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './User/user/user.module';  
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/MoneyMap'),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [],  
+  providers: [],    
 })
 export class AppModule {}
